7003b02656c4ce0f53328ff2e3f79624d6603e6b
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-09-28 17:28:03 -0300

    added more posts and organized code

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -24,6 +24,18 @@ export class AppComponent {
       imageUrl: 'assets/biking.jpeg',
       username: 'biking12222',
       content: 'I did some biking today!'
-    }
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain :D'
+    },
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today ^'
+    },
   ];
 }
